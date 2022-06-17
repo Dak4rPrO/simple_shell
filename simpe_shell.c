f7ac444361eb30a3789de623bc282889dc5edc58
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-17 17:27:09 -0300

    Create simpe_shell.c

---
+++ simpe_shell.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * main - simple and basic shell
+ * Return: return the size of the buffer
+ */
+
+int main(void)
+{
+	char *buffer = NULL, *token = NULL, *tokens[1024];
+	size_t bufsize = 0;
+	int characters, i = 0, retorno = 0;
+
+	while (1)
+	{
+		if (isatty(0))
+			write(1, "$ ", 2);
+		buffer = NULL;
+		bufsize = 0;
+		characters = getline(&buffer, &bufsize, stdin);
+		if (characters == -1)
+			break;
+		/* buffer[_strlen(buffer) - 1] = '\0'; */
+		token = strtok(buffer, " \n\t");
+		if (!token || (_strcmp(token, "exit") == 0))	/*exit*/
+			break;
+		if (!token || (_strcmp(token, "env") == 0)) /*env*/
+			_getenv();
+
+		i = 0;
+		while (token != NULL)
+		{
+			tokens[i] = token;
+			tokens[i + 1] = NULL;
+			token = strtok(NULL, " \n\t");
+			i++;
+		}
+
+    retorno = execute(tokens);
+		free(buffer);
+	}
+	free(buffer);
+	return (retorno);
+}
