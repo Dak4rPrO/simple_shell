db40d367f7470e68075d818599607a9cea117ff5
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-17 17:10:50 -0300

    Create aux_func.c
    Functions.

---
+++ aux_func.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string.
+* @s: string.
+* Return: length of *s (string).
+*/
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+
+/**
+* _strcmp - compares two strings.
+* @s1: first string.
+* @s2: second string.
+* Description: show "0" if the two values are equal.
+* Return: Always 0 (Success).
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int cont, val;
+
+	cont = 0;
+	while (s1[cont] == s2[cont] && s1[cont] != '\0')
+		cont++;
+
+	val = s1[cont] - s2[cont];
+	return (val);
+}
+
+/**
+* _getenv - prints the current environment.
+* Return: Always 0 (Success).
+*/
+
+int _getenv(void)
+{
+	unsigned int i;
+	unsigned int j = 0;
+
+	for (i = 0; environ[i] != NULL; i++)
+  {
+		write(1, environ[j], _strlen(environ[j]));
+		_putchar('\n');
+		j++;
+	}
+	return (0);
+}
+
+/**
+* _putchar - writes the character c to stdout.
+* @c: The character to print.
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
