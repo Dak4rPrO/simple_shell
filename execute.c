fca04db9c053813fb7c379952d9f634cad552a59
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-17 17:14:39 -0300

    Create execute.c

---
+++ execute.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * execute - Function in charge of looking for a given command,
+ * if it finds it, it 'forks' it and executes it.
+ * @cmd: given command.
+ * Return: If the execute function works correctly, 0 will be returned.
+ */
+
+int execute(char **cmd)
+{
+	pid_t child_pid;
+	int status = 0;
+
+	child_pid = fork();
+
+	if (child_pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	else if (child_pid == 0)
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+	}
+	else
+    wait(&status);
+	return (WEXITSTATUS(status));
+}
